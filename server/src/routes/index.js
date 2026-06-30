01b743482ec3e1773e21d1854d88858a7e05dd42
Shivank Lavania <shivanklavania6@gmail.com>
2026-06-30 12:38:16 +0530

    Add update route for skills

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,7 +4,7 @@ import verifyToken from "../middleware/verifyToken.js";
 
 import { adminLogin, changePassword, logout, addAdmin } from "../controllers/auth.controller.js";
 import { getProfile, editProfile } from "../controllers/profile.controller.js";
-import { getAllSkills, addSkill, deleteSkill } from "../controllers/skill.controller.js";
+import { getAllSkills, addSkill, updateSkill, deleteSkill } from "../controllers/skill.controller.js";
 import { getAllProjects, addProject, deleteProject } from "../controllers/project.controller.js";
 import { getAllExperiences, addExperience, deleteExperience } from "../controllers/experience.controller.js";
 import { getAllEducation, addEducation, deleteEducation } from "../controllers/education.controller.js";
@@ -31,6 +31,7 @@ router.put("/profile", verifyToken, editProfile);
 ========================= */
 router.get("/skills", getAllSkills);
 router.post("/skills", verifyToken, addSkill);
+router.put("/skills/:id", verifyToken, updateSkill);   // Edit skill
 router.delete("/skills/:id", verifyToken, deleteSkill);
 
 /* =========================
